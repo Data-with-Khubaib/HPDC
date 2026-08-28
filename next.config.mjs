9fe5c544e9a9c5b3368abc8895ae27e2212098ef
Data-khubaib <khubaibshafiq5@gmail.com>
2026-08-28 19:49:51 +0500

    Fixes Bugs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export', // Forces Next.js to build static files into an 'out' folder
-  basePath: '/HPDC', // Case-sensitive. Matches your repository name exactly
-  images: {
-    unoptimized: true, // Prevents image optimization errors during export
-  },
 };
 
 export default nextConfig;
